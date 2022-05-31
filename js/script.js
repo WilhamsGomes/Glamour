9b34d96925ec14f72f27f64b1ab05f1bb1d8b989
WilhamsGomes <wilhamsgoomes@gmail.com>
2022-05-31 17:42:38 -0300

    Enviando p deploy

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const tituloCliente = document.getElementById('numbers-test');
+
+function escreverTxt(elemento){
+
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+
+    textoArray.forEach( (letra, i) =>{
+        setTimeout(function(){ 
+            elemento.innerHTML += letra
+            elemento.style.color = 'red'
+        }, 150 * i) 
+    });
+
+}
+
+//escreverTxt(tituloCliente)
+
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+  counter.innerText = '0';
+  
+  sinal = " + " ;
+
+  const updateCounter = () =>{
+    const target = +counter.getAttribute('data-target');
+    const c = +counter.innerText;
+
+    const increment = target / 600;
+    
+    if (c < target){
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+
+  }
+  updateCounter();
+});
+
